c72e11e0ecd77b1438e80761b834f18894390097
Yann SEGET <dev@leafgard.fr>
2020-06-23 14:18:59 +0200

    fix(orm): made config dynamic

--- ormconfig.js
+++ ormconfig.js
@@ -1,22 +1,24 @@
-const { config } = require('dotenv')
+const {config} = require('dotenv')
 config()
 
+const isProd = process.env.NODE_ENV === 'production'
+
 module.exports = {
-  type: process.env.DB_TYPE || "mysql",
-  host: process.env.DB_HOST || "localhost",
-  port: process.env.DB_PORT || 3306,
-  username: process.env.DB_USER || "test",
-  password: process.env.DB_PASS || "test",
-  database: process.env.DB_NAME || "test",
-  synchronize: true,
-  logging: false,
-  entities: [
-    "src/entity/**/*.ts"
-  ],
-  migrations: [
-    "src/migration/**/*.ts"
-  ],
-  subscribers: [
-    "src/subscriber/**/*.ts"
-  ]
+    type: process.env.DB_TYPE || "mysql",
+    host: process.env.DB_HOST || "localhost",
+    port: process.env.DB_PORT || 3306,
+    username: process.env.DB_USER || "test",
+    password: process.env.DB_PASS || "test",
+    database: process.env.DB_NAME || "test",
+    synchronize: true,
+    logging: false,
+    entities: [
+        `${isProd ? 'dist' : 'src'}/entity/**/*.${isProd ? 'js' : 'ts'}`
+    ],
+    migrations: [
+        `${isProd ? 'dist' : 'src'}/migration/**/*.${isProd ? 'js' : 'ts'}`
+    ],
+    subscribers: [
+        `${isProd ? 'dist' : 'src'}/subscriber/**/*.${isProd ? 'js' : 'ts'}`
+    ]
 }
