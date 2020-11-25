e9354f77623df657e615f2ddbc01c1c26b448293
Yann SEGET <dev@leafgard.fr>
2020-11-25 23:07:14 +0100

    secu(app): turned ormconfig into example config

--- ormconfig.example.js
+++ ormconfig.example.js
@@ -1,15 +1,25 @@
 const {config} = require('dotenv')
 config()
 
-const isProd = process.env.NODE_ENV === 'production'
+const {
+    NODE_ENV,
+    DB_TYPE,
+    DB_HOST,
+    DB_PORT,
+    DB_USER,
+    DB_PASS,
+    DB_NAME
+} = process.env
+
+const isProd = NODE_ENV === 'production'
 
 module.exports = {
-    type: process.env.DB_TYPE || "mysql",
-    host: process.env.DB_HOST || "localhost",
-    port: process.env.DB_PORT || 3306,
-    username: process.env.DB_USER || "test",
-    password: process.env.DB_PASS || "test",
-    database: process.env.DB_NAME || "test",
+    type: DB_TYPE || "mysql",
+    host: DB_HOST || "localhost",
+    port: DB_PORT || 3306,
+    username: DB_USER || "test",
+    password: DB_PASS || "test",
+    database: DB_NAME || "test",
     synchronize: true,
     logging: false,
     entities: [
